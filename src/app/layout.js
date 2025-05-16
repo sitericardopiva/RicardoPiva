4d7a9990378e0f3475340462ea0e284dee09f500
joaovlummertz <joaolummertz1@gmail.com>
2025-05-15 22:50:20 -0300

    Template cleanup

--- src/app/layout.js
+++ src/app/layout.js
@@ -66,7 +66,7 @@ export default function RootLayout({ children }) {
           src="https://identity.netlify.com/v1/netlify-identity-widget.js"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"></link>
         <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"></link>
       </head>
       <body
