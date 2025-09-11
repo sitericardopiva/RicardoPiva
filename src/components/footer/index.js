a3c9557115ff9644897c913a395cafa7a34fe0d3
Joao-Scheffel-Koller <joaoscheffelkoller@gmail.com>
2025-09-11 19:12:53 -0300

    Logo IDE

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,11 +2,14 @@
 
 import styles from "./footer.module.css";
 import { FaMapMarkerAlt, FaEnvelope, FaPhone } from "react-icons/fa";
+import Image from "next/image";
+import logoIDE from "../../../public/uploads/IDE_logo.png";
 
 export default function Footer() {
   const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
 
   return (
+    <>
     <div className={styles.footer}>
       <div className={styles.container}>
         <h1>Endereço</h1>
@@ -42,5 +45,20 @@ export default function Footer() {
         </div>
       </div>
     </div>
+
+    <div className={styles.developedBy}>
+      <a href="https://idejr.com.br/" className={styles.developedByLink} target = "_blank">
+        <div className={styles.developedByCover}>
+          <Image
+            src={logoIDE}
+            alt="Logo da IDE Jr"
+            className={styles.ideLogo}
+          />
+        </div>
+        <p className = {styles.developedByText}>Desenvolvido por IDE - UFRGS</p>
+    </a>
+  </div>
+
+  </>
   );
 }
